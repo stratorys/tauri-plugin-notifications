0ceaa33e6699651bfa2c071cc251376ef436bf19
Manuel Philipp <manuel.philipp@freshx.de>
2025-02-04 15:12:57 +0100

    android notifications

---
+++ packages/tauri-plugin-notifications/android/src/main/java/NotificationsPlugin.kt
@@ -0,0 +1,71 @@
+package com.plugin.notifications
+
+import android.Manifest
+import android.app.Activity
+import android.util.Log
+import android.webkit.WebView
+import app.tauri.annotation.Command
+import app.tauri.annotation.InvokeArg
+import app.tauri.annotation.Permission
+import app.tauri.annotation.PermissionCallback
+import app.tauri.annotation.TauriPlugin
+import app.tauri.plugin.Invoke
+import app.tauri.plugin.JSObject
+import app.tauri.plugin.Plugin
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
+
+
+
+@TauriPlugin(
+    permissions = [
+        Permission(strings = [Manifest.permission.POST_NOTIFICATIONS], alias = "postNotification")
+    ]
+)
+class NotificationsPlugin(private val activity: Activity): Plugin(activity) {
+    @Command
+    fun initFirebase(invoke: Invoke){
+        Log.i("notifyPlugin", "initializing firebase")
+        FirebaseApp.initializeApp(this.activity)
+    }
+
+    @Command
+    override fun requestPermissions(invoke: Invoke){
+        this.requestPermissionForAlias("postNotification",invoke,"notifyPermissionRequestDone");
+    }
+
+    @PermissionCallback
+    fun notifyPermissionRequestDone(invoke: Invoke){
+        Log.i("notifyPlugin","Permission request done")
+        val ret = JSObject()
+        ret.put("status",getPermissionState("postNotification"))
+        invoke.resolve(ret)
+    }
+
+    @Command
+    fun registerForRemoteNotifications(invoke: Invoke){
+        Log.i("notifyPlugin", "Registering for notifications");
+        FirebaseMessaging.getInstance().getToken()
+            .addOnCompleteListener { task ->
+                val ret = JSObject();
+                if (!task.isSuccessful) {
+                    Log.e("notifyPlugin", "Could not get firebase token.")
+                    ret.put("success", false)
+                    ret.put("error", "Failed to get firebase token");
+                } else{
+                    // Get new Instance ID token
+                    val token: String = task.result
+                    ret.put("success",true)
+                    ret.put("token", token)
+                }
+
+
+                invoke.resolve(ret)
+            }
+            .addOnCanceledListener {
+                invoke.reject("Task cancelled")
+            }
+    }
+}
--- packages/tauri-plugin-notifications/src/mobile.rs
+++ packages/tauri-plugin-notifications/src/mobile.rs
@@ -1,9 +1,9 @@
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use tauri::{
-  ipc::Channel,
-  plugin::{PluginApi, PluginHandle},
-  AppHandle, Runtime,
+    ipc::Channel,
+    plugin::{PluginApi, PluginHandle},
+    AppHandle, Runtime,
 };
 
 use crate::models::*;
@@ -12,56 +12,55 @@ use crate::models::*;
 tauri::ios_plugin_binding!(init_plugin_notifications);
 
 pub fn init<R: Runtime, C: DeserializeOwned>(
-  _app: &AppHandle<R>,
-  api: PluginApi<R, C>,
+    _app: &AppHandle<R>,
+    api: PluginApi<R, C>,
 ) -> crate::Result<Notifications<R>> {
-  #[cfg(target_os = "ios")]
-  let handle = api.register_ios_plugin(init_plugin_notifications)?;
-  Ok(Notifications(handle))
+    #[cfg(target_os = "ios")]
+    let handle = api.register_ios_plugin(init_plugin_notifications)?;
+    #[cfg(target_os = "android")]
+    let handle = api.register_android_plugin("com.plugin.notifications", "NotificationsPlugin")?;
+    #[cfg(target_os = "android")]
+    handle.run_mobile_plugin("initFirebase", ())?;
+    Ok(Notifications(handle))
 }
 
 pub struct Notifications<R: Runtime>(PluginHandle<R>);
 
 #[derive(Serialize)]
 struct WatchNotificationsArgs {
-  channel: Channel,
+    channel: Channel,
 }
 
 impl<R: Runtime> Notifications<R> {
-  pub fn check_permissions(&self) -> crate::Result<NotificationPermissionStatus> {
-    self
-      .0
-      .run_mobile_plugin("checkPermissions", ())
-      .map_err(Into::into)
-  }
+    pub fn check_permissions(&self) -> crate::Result<NotificationPermissionStatus> {
+        self.0
+            .run_mobile_plugin("checkPermissions", ())
+            .map_err(Into::into)
+    }
 
-  pub fn request_permissions(&self) -> crate::Result<NotificationPermissionStatus> {
-    self
-      .0
-      .run_mobile_plugin("requestPermissions", ())
-      .map_err(Into::into)
-  }
+    pub fn request_permissions(&self) -> crate::Result<NotificationPermissionStatus> {
+        self.0
+            .run_mobile_plugin("requestPermissions", ())
+            .map_err(Into::into)
+    }
 
-  pub fn check_registration_status(&self) -> crate::Result<NotificationRegistrationStatus> {
-    self
-      .0
-      .run_mobile_plugin("checkRegistrationStatus", ())
-      .map_err(Into::into)
-  }
+    pub fn check_registration_status(&self) -> crate::Result<NotificationRegistrationStatus> {
+        self.0
+            .run_mobile_plugin("checkRegistrationStatus", ())
+            .map_err(Into::into)
+    }
 
-  pub fn register_for_remote_notifications(&self) -> crate::Result<NotificationRegistrationResult> {
-    self
-      .0
-      .run_mobile_plugin("registerForRemoteNotifications", ())
-      .map_err(Into::into)
-  }
+    pub fn register_for_remote_notifications(
+        &self,
+    ) -> crate::Result<NotificationRegistrationResult> {
+        self.0
+            .run_mobile_plugin("registerForRemoteNotifications", ())
+            .map_err(Into::into)
+    }
 
-  pub fn watch_notifications(&self, channel: Channel) -> crate::Result<WatchNotificationResult> {
-    self.0
-      .run_mobile_plugin(
-        "watchNotifications",
-        WatchNotificationsArgs { channel },
-      )
-      .map_err(Into::into)
-  }
+    pub fn watch_notifications(&self, channel: Channel) -> crate::Result<WatchNotificationResult> {
+        self.0
+            .run_mobile_plugin("watchNotifications", WatchNotificationsArgs { channel })
+            .map_err(Into::into)
+    }
 }
